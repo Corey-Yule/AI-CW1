1550739d707e64a3d16797b17ec5d0ce2ee1464b
Corey <87344090+Corey-Yule@users.noreply.github.com>
2025-09-26 14:33:40 +0100

    GRID creation Testing

---
+++ PartA&B/Part1A.py
@@ -0,0 +1,23 @@
+#Coursework 1 - Corey Yule, Euan Grierson 
+#Part 1A
+
+#This is one single 3x3 GRID (NOTE: We Need 9 of these for 81 total boxes for values)
+GRID = {
+    1:0,
+    2:0,
+    3:0,
+    4:0,
+    5:0,
+    6:0,
+    7:0,
+    8:0,
+    9:0
+}
+
+#Assign random values in Certain boxes.
+def assignGridVals(GRID):
+    for x in GRID :
+        print(x)
+
+
+assignGridVals(GRID)
