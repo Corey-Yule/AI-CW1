c3a4ac0858b11fa42b41816adaa2a0ec83a3c73e
Corey-Yule <coreyyule22@gmail.com>
2025-09-29 13:12:22 +0100

    Rules finished and test cases made.

--- PartA&B/Part1B.py
+++ PartA&B/Part1B.py
@@ -3,10 +3,11 @@
 import math
 import csv
 import numpy as np
+import io
 class SudokuCSP:
     def __init__(self, grid, domains):
         #Defines a grid of 81 squares (NOTE: this is currently fully empty as 0 defines as an empty square)
-        self.grid = grid.flatten()
+        self.grid = grid
         self.domains = {}
         #Loop over each square in the grid this sorts the domain of hidden numbers
         for r in range(9):
@@ -18,33 +19,35 @@ class SudokuCSP:
 
     def get_puzzle(file):
         #open file in read mode
-        datafile = open(file, 'r')
+        with open(file, 'r') as datafile:
         #read the file with a comma delimiter
-        reader=csv.reader(datafile, delimiter=',')
-        #define the grid as an empty array
-        grid=[]
-        #for each row in the file, append to the grid
-        for row in reader:
-            grid.append(row)
+            reader = csv.reader(datafile, delimiter=',', skipinitialspace=True)
+
+            #define the grid as an empty array
+            grid=[]
+            #for each row in the file, append to the grid
+            for row in reader:
+                grid.append([int(x) for x in row])
+            #this allows it to be read in as an int instead of a string
         return grid
         
-
     #Checks constraints -> 3x3 no dups, rows no dupes, no dupes
     def rule_check(self, row, col, val, grid):
-        #Check Row
-        for r in range(9):
-            if r!= row and grid[r][col] == val:
-                return False
-        #Check Column
+        # Check Row
         for c in range(9):
-            if c!= col and grid[row][c] == val:
+            if c != col and grid[row][c] == val:
                 return False
+        # Check Column
+        for r in range(9):
+            if r != row and grid[r][col] == val:
+                return False
+
         #Check 3x3
         box_row = (row // 3)*3
         box_col = (col // 3)*3
 
-        for r in(box_row, box_row + 3):
-            for c in(box_col, box_col + 3):
+        for r in range(box_row, box_row + 3):
+            for c in range(box_col, box_col + 3):#loop over the 2D array
                 if(r != row or c != col) and grid[r][c] == val:
                     return False
                 
@@ -53,4 +56,7 @@ class SudokuCSP:
 
 # #Visual thing for my brain to work.
 grid = SudokuCSP.get_puzzle("test.csv")
-print(SudokuCSP.rule_check(0, 1, 5, grid))
+sudoku = SudokuCSP(grid, {})   # create the board
+print(sudoku.rule_check(0, 2,1, grid)) #should return True
+print(sudoku.rule_check(0, 2,5, grid)) #should return False
+
