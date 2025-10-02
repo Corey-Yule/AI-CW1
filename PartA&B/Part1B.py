5546b5a5379cbd605c4c4bfc3d6ad6e441e2731e
Corey-Yule <coreyyule22@gmail.com>
2025-10-02 14:52:37 +0100

    sizes + dimensions work on linux now and background colour change

--- PartA&B/Part1B.py
+++ PartA&B/Part1B.py
@@ -1,6 +1,8 @@
 #Coursework 1 - Corey Yule, Euan Grierson 
 #Part 1A
 
+#Reference: https://www.geeksforgeeks.org/dsa/sudoku-backtracking-7/ <- this is for our report.
+
 #Imports
 import csv
 import numpy as np
@@ -58,7 +60,8 @@ class SudokuGui:
     def __init__(self,main,grid):
         self.main = main
         self.main.title("Sudoku Puzzle Solver")
-        self.main.geometry("560x625") #Size
+        self.main.geometry("600x750") #Size
+        main.configure(bg='lightblue')
 
         #Creating the sudoku board
         self.grid = grid
@@ -73,10 +76,10 @@ class SudokuGui:
                 #if the value is 0 show as blank
                 text = str(value) if value != 0 else " "
                 #Label Setup
-                label = tk.Label(main, text=text, width=4, height=2, font=("Ariel", 18), relief="solid", borderwidth=1)
-                label.grid(row=r, column=c, padx=1, pady=1)
+                label_boxes = tk.Label(main, text=text, width=4, height=2, font=("Ariel", 18), relief="solid", borderwidth=1)
+                label_boxes.grid(row=r, column=c, padx=1, pady=1)
                 #row cells
-                row_cell.append(label)
+                row_cell.append(label_boxes)
             self.boxes.append(row_cell)
 
         solve_button = tk.Button(root,text="Solve Puzzle",command=self.solve_puzzle, bg="#4CAF50",fg="white",
@@ -94,6 +97,7 @@ class SudokuGui:
     def solve_puzzle(self):
         print("Solve button clicked!")
         # backtracking function here with the use of the rule check.
+
 
 #main
 grid = SudokuCSP.get_puzzle("test.csv")
